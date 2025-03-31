172ffb4702ef227a5cc526314dafd39a922d97b5
iain <iain@soulflyer.co.uk>
2025-03-31 23:20:16 +0700

    Detect all 6 axes

--- 3d-midi.py
+++ 3d-midi.py
@@ -23,33 +23,88 @@ def output_ports():
 
 
 def button_0(state, buttons, pressed_buttons):
+    """Event fired when button 0 is pressed."""
     print("Button 0 pressed:", pressed_buttons)
 
 
 def button_1(state, buttons, pressed_buttons):
+    """Event fired when button 1 is pressed."""
     print("Button 1 pressed", pressed_buttons)
 
 
-def button_0_1(state, buttons, pressed_buttons):
-    print("Both buttons pressed:", pressed_buttons)
+def x_axis(state, axis):
+    """Event fired when x-axis moves."""
+    print(f"X-axis: {axis}")
 
 
-def someButton(state, buttons):
-    print("Some button event")
+def y_axis(state, axis):
+    """Event fired when y-axis moves."""
+    print(f"Y-axis: {axis}")
 
 
-def callback():
+def z_axis(state, axis):
+    """Event fired when z-axis moves."""
+    print(f"Z-axis: {axis}")
+
+
+def roll(state, axis):
+    """Event fired on roll."""
+    print(f"Roll: {axis}")
+
+
+def pitch(state, axis):
+    """Event fired on pitch."""
+    print(f"Pitch: {axis}")
+
+
+def yaw(state, axis):
+    """Event fired on yaw."""
+    print(f"Yaw: {axis}")
+
+
+def main():
+    """Listen to the 3d mouse and fire various fns."""
     button_arr = [pyspacemouse.ButtonCallback([0], button_0),
                   pyspacemouse.ButtonCallback([1], button_1),]
+    axis_arr = [pyspacemouse.DofCallback("x",
+                                         x_axis,
+                                         0.01,
+                                         x_axis,
+                                         0.0001),
+                pyspacemouse.DofCallback("y",
+                                         y_axis,
+                                         0.01,
+                                         y_axis,
+                                         0.0001),
+                pyspacemouse.DofCallback("z",
+                                         z_axis,
+                                         0.01,
+                                         z_axis,
+                                         0.0001),
+                pyspacemouse.DofCallback("roll",
+                                         roll,
+                                         0.01,
+                                         roll,
+                                         0.0001),
+                pyspacemouse.DofCallback("pitch",
+                                         pitch,
+                                         0.01,
+                                         pitch,
+                                         0.0001),
+                pyspacemouse.DofCallback("yaw",
+                                         yaw,
+                                         0.01,
+                                         yaw,
+                                         0.0001)]
 
-    success = pyspacemouse.open(dof_callback=pyspacemouse.print_state,
-                                button_callback_arr=button_arr)
+    success = pyspacemouse.open(button_callback_arr=button_arr,
+                                dof_callback_arr=axis_arr)
     if success:
         while True:
             pyspacemouse.read()
             time.sleep(0.01)
 
 
-callback()
+main()
 # output_ports()
 # read_spacemouse()
