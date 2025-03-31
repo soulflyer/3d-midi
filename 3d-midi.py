24ca5d88213912c5efd5753deca73fa6c9e23a3b
iain <iain@soulflyer.co.uk>
2025-03-31 18:59:00 +0700

    Detect midi ports and 3d mouse input

--- 3d-midi.py
+++ 3d-midi.py
@@ -1,5 +1,17 @@
 """Functions to translate 3d mouse input to midi."""
 import rtmidi
+import pyspacemouse
+import time
+
+
+def read_spacemouse():
+    """Output values from the spacemouse."""
+    success = pyspacemouse.open(dof_callback=pyspacemouse.print_state,
+                                button_callback=pyspacemouse.print_buttons)
+    if success:
+        while 1:
+            pyspacemouse.read()
+            time.sleep(0.01)
 
 
 def output_ports():
@@ -11,3 +23,4 @@ def output_ports():
 
 
 output_ports()
+read_spacemouse()
